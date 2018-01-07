ba7f68b612d3e15cbe4ffca732ae49e3054a4016
egbert.yi <java_egbert@163.com>
2018-01-07 23:06:03 +0800

    add nginx ip collector

--- collector/nginx_collector.py
+++ collector/nginx_collector.py
@@ -9,31 +9,48 @@ from datetime import datetime, timedelta
 from config.mysql import conn
 
 
-NGINX = {
-    'app': 'nginx',
-    'log_file': '/shiheng/logs/nginx/access.log'
-}
 NGINX_LOG_FORMAT = '%d/%b/%Y:%H:%M'
-apps = [
-    {
-        'app_name': 'trade',
-        'app_key': '127.0.0.1:8081'
-    },
-    {
-        'app_name': 'trade2',
-        'app_key': '127.0.0.1:8080'
-    }
-]
 
+nginx = {
+    'ip': '39.108.237.190',
+    'log_file': '/shiheng/logs/nginx/access.log',
+    'apps': [
+        {
+            'app_name': 'trade',
+            'app_key': '127.0.0.1:8081',
+            'collect_type': False,
+            'collect_ip': False,
+        },
+        {
+            'app_name': 'trade2',
+            'app_key': '127.0.0.1:8080',
+            'collect_type': True,
+            'collect_ip': True
+        }
+    ]
+}
+
+'''
+nginx 日志格式应为
+        log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                            '$status $body_bytes_sent "$http_referer" '
+                            '"$http_user_agent" "$http_x_forwarded_for"'
+                            '$upstream_addr $upstream_response_time $request_time ';
+'''
 
 SHELL_COMMAND = 'grep "%s" %s | grep "%s" | wc -l'
 SHELL_COMMAND_TYPE = 'grep "%s" %s | grep "%s" | grep "%s" | wc -l'
+SHELL_COMMAND_IP = 'grep "%s" %s | grep "%s" | awk "{print $1}" | sort | uniq | wc -l'
 
 
-def collect_nginx_by_key(app, time_key):
+
+def collect_nginx_by_app(app, time_key):
+    '''
+    app 访问数量
+    '''
     app_name = app.get('app_name')
     app_key = app.get('app_key')
-    log_file = NGINX.get('log_file')
+    log_file = nginx.get('log_file')
     command = SHELL_COMMAND % (time_key, log_file, app_key)
     output = os.popen(command)
     num = output.readlines()[0]
@@ -46,13 +63,16 @@ def collect_nginx_by_key(app, time_key):
         'time': stime.strftime("%Y-%m-%d %H:%M:00"),
         'app': app_name,
         'num': ss_num,
-        'ip': '39.108.237.190'
+        'ip': nginx.get('ip')
     }
 
 def collect_nginx_by_type(app, time_key, type):
+    '''
+    不同类型[android,ios]对app的访问数量
+    '''
     app_name = app.get('app_name')
     app_key = app.get('app_key')
-    log_file = NGINX.get('log_file')
+    log_file = nginx.get('log_file')
     command = SHELL_COMMAND_TYPE % (time_key, log_file, app_key, type)
     output = os.popen(command)
     num = output.readlines()[0]
@@ -65,28 +85,33 @@ def collect_nginx_by_type(app, time_key, type):
         'time': stime.strftime("%Y-%m-%d %H:%M:00"),
         'app': app_name+'-'+type,
         'num': ss_num,
-        'ip': '39.108.237.190'
+        'ip': nginx.get('ip')
     }
 
-def collect_data():
-    now = datetime.now()
-    ## 统计上一分钟
-    first_min = now - timedelta(minutes=1)
-    time_str = first_min.strftime('%d/%b/%Y:%H:%M')
-    collect_data_by_date(time_str)
-
-
-def collect_data_by_date(time):
+def collect_nginx_by_ip(app, time_key):
     '''
-    :param time: 'yyyy-mm-dd HH:MM'
-    :return:
+    对app的访问ip的数量
     '''
-    datas = [collect_nginx_by_key(app, time) for app in apps]
-    types = ['Android', 'iPhone']
-    type_datas = [collect_nginx_by_type(apps[1], time, type) for type in types]
-    push_data_to_mysql(datas, type_datas)
+    app_name = app.get('app_name')
+    app_key = app.get('app_key')
+    log_file = nginx.get('log_file')
+    command = SHELL_COMMAND_IP % (time_key, log_file, app_key)
+    output = os.popen(command)
+    num = output.readlines()[0]
+    ss_num = int(num.strip())
 
-def push_data_to_mysql(datas, type_datas):
+    stime = datetime.strptime(time_key, NGINX_LOG_FORMAT)
+
+    print command, ss_num
+    return {
+        'time': stime.strftime("%Y-%m-%d %H:%M:00"),
+        'app': app_name,
+        'num': ss_num,
+        'ip': nginx.get('ip')
+    }
+
+
+def push_data_to_mysql(datas, type_datas, ip_datas):
     cur = conn.cursor()
     try:
         sql = '''insert into nginx_num_m(time, app, num, ip)
@@ -95,21 +120,58 @@ def push_data_to_mysql(datas, type_datas):
         values = [(data.get('time'), data.get('app'), int(data.get('num')), data.get('ip')) for data in datas]
         cur.executemany(sql,values)
 
-        type_sql = '''insert into nginx_num_type(time, app, num, ip)
-                values(%s, %s, %s, %s) ON DUPLICATE KEY
-                UPDATE time=VALUES(time) and app=VALUES(app) and ip=VALUES(ip)'''
-        types_values = [(data.get('time'), data.get('app'), int(data.get('num')), data.get('ip')) for data in type_datas]
-        cur.executemany(type_sql, types_values)
+        if type_datas:
+            type_sql = '''insert into nginx_num_type(time, app, num, ip)
+                    values(%s, %s, %s, %s) ON DUPLICATE KEY
+                    UPDATE time=VALUES(time) and app=VALUES(app) and ip=VALUES(ip)'''
+            types_values = [(data.get('time'), data.get('app'), int(data.get('num')), data.get('ip')) for data in type_datas]
+            cur.executemany(type_sql, types_values)
+
+        if ip_datas:
+            ip_sql = '''insert into nginx_num_ip(time, app, num, ip)
+                    values(%s, %s, %s, %s) ON DUPLICATE KEY
+                    UPDATE time=VALUES(time) and app=VALUES(app) and ip=VALUES(ip)'''
+            ip_values = [(data.get('time'), data.get('app'), int(data.get('num')), data.get('ip')) for data in ip_datas]
+            cur.executemany(ip_sql, ip_values)
 
     except Exception,ex:
         print ex
         return False
     finally:
         conn.commit()
-
         cur.close()
         conn.close()
     #print values
     return True
 
 
+def collect_data_by_date(time):
+    '''
+    :param time: 'yyyy-mm-dd HH:MM'
+    :return:
+    '''
+    types = ['Android', 'iPhone']
+    apps = nginx.get('apps')
+    datas = [collect_nginx_by_app(app, time) for app in apps]
+    type_datas = []
+    ip_datas = []
+    for app in apps:
+        collect_type = app.get('collect_type')
+        collect_ip = app.get('collect_ip')
+        if collect_type:
+            types = [collect_nginx_by_type(app, time, type) for type in types]
+            type_datas.extend(types)
+
+        if collect_ip:
+            ip_data = collect_nginx_by_ip(app, time)
+            ip_datas.append(ip_data)
+
+    push_data_to_mysql(datas, type_datas, ip_datas)
+
+
+def collect_data():
+    now = datetime.now()
+    ## 统计上一分钟
+    first_min = now - timedelta(minutes=1)
+    time_str = first_min.strftime('%d/%b/%Y:%H:%M')
+    collect_data_by_date(time_str)
